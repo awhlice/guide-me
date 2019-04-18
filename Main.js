51e33aa64cdc939a84b57d7d6a63ed8c0b871a33
awhlice <alicegwu@gmail.com>
2019-04-18 13:51:00 -0400

    uploaded Main.js

---
+++ Main.js
@@ -0,0 +1,9 @@
+const {app, BrowserWindow} = require('electron')      
+
+function createWindow () {   
+  win = new BrowserWindow({width: 1440, height: 1024}) 
+  win.loadURL('http://localhost:3000/')       
+  //win.webContents.openDevTools()   
+}
+
+app.on('ready', createWindow) 
